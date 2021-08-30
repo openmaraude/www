b67c603517c01ca7c453d69812207c427f4f679f
Julien Castets <castets.j@gmail.com>
2021-08-30 18:38:21 +0200

    Add section to groups page

--- pages/groups.jsx
+++ pages/groups.jsx
@@ -9,42 +9,81 @@ import styles from '../styles/Groups.module.css';
 export default function GroupsPage() {
   return (
     <BaseLayout>
-      <section className={styles.goalsSection}>
-        <div className={styles.goalsSectionLeft}>
-          <h2>
-            <span class="highlight">Connectez vos taxis</span> à la maraude électronique
-          </h2>
+      <div className={styles.page}>
+        <section className={styles.goalsSection}>
+          <div className={styles.goalsSectionLeft}>
+            <h2>
+              <span class="highlight">Connectez vos taxis</span> à la maraude électronique
+            </h2>
 
-          <div className={styles.goalsPresentationBox}>
-            le.taxi, le <strong>service public numérique</strong> qui permet de
-            trouver de <strong>nouveaux clients</strong> avec
-            la <strong>maraude électronique</strong>.
-          </div>
-        </div>
-
-        <div className={styles.goalsSectionRight}>
-          <div className={styles.goalsIllustration}>
-            <div className={styles.goalsIllustrationContainer}>
-              <Image src="/images/groupspage/illustration-customers.svg" alt="Illustration clients" layout="fill" />
+            <div className={styles.goalsPresentationBox}>
+              le.taxi, le <strong>service public numérique</strong> qui permet de
+              trouver de <strong>nouveaux clients</strong> avec
+              la <strong>maraude électronique</strong>.
             </div>
           </div>
 
-          <h4 class="highlight">
-            Nos ambitions pour les groupements de taxis
-            <span className={styles.titleBorder} />
-          </h4>
+          <div className={styles.goalsSectionRight}>
+            <div className={styles.goalsIllustration}>
+              <div className={styles.goalsIllustrationContainer}>
+                <Image src="/images/groupspage/illustration-customers.svg" alt="Illustration clients" layout="fill" />
+              </div>
+            </div>
 
-          <ul>
-            <li><strong>Accompagner la modernisation de l’offre de taxis</strong></li>
-            <li><strong>Encore plus de courses pour les taxis</strong></li>
-            <li><strong>Votre clientèle trouve un taxi partout en France, via votre application</strong></li>
-          </ul>
+            <h4 class="highlight">Nos ambitions pour les groupements de taxis</h4>
 
-          <div className={styles.goalsCTA}>
-            <ButtonLink href="#">S'inscrire</ButtonLink>
+            <ul>
+              <li><strong>Accompagner la modernisation de l’offre de taxis</strong></li>
+              <li><strong>Encore plus de courses pour les taxis</strong></li>
+              <li><strong>Votre clientèle trouve un taxi partout en France, via votre application</strong></li>
+            </ul>
+
+            <div className={styles.goalsCTA}>
+              <ButtonLink href="#">S'inscrire</ButtonLink>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+
+
+        <section className={styles.advantagesSection}>
+          <h4 class="highlight">Les avantages de la maraude électronique</h4>
+
+          <div className={styles.advantageCards}>
+            <div className={styles.advantageCard}>
+              <div className={styles.advantageIllustration}>
+                <div className={styles.advantageIllustrationContainer}>
+                  <Image src="/images/groupspage/illustration-client-app.svg" alt="Illustration cliente utilisant une app" layout="fill" />
+                </div>
+              </div>
+              <div className={styles.advantageText}>
+                <span class="highlight">Un taxi à moins de 500m</span> sans frais d’approche
+              </div>
+            </div>
+
+            <div className={styles.advantageCard}>
+              <div className={styles.advantageIllustration}>
+                <div className={styles.advantageIllustrationContainer}>
+                  <Image src="/images/groupspage/illustration-clock.svg" alt="Illustration montre" layout="fill" />
+                </div>
+              </div>
+              <div className={styles.advantageText}>
+                <span class="highlight">Optimiser le temps</span> entre 2 réservations
+              </div>
+            </div>
+
+            <div className={styles.advantageCard}>
+              <div className={styles.advantageIllustration}>
+                <div className={styles.advantageIllustrationContainer}>
+                  <Image src="/images/groupspage/illustration-map.svg" alt="Illustration carte" layout="fill" />
+                </div>
+              </div>
+              <div className={styles.advantageText}>
+                <span class="highlight">Un meilleur accès aux taxis</span> dans toute la France
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </BaseLayout>
   );
 }
