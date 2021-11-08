3dc16c7eb0c99a24c16e19360b5b50d878c611b7
Julien Castets <castets.j@gmail.com>
2021-11-08 15:27:00 +0100

    Add 404 custom page

---
+++ pages/404.jsx
@@ -0,0 +1,17 @@
+import BaseLayout from '@/layouts/BaseLayout';
+
+import styles from '@/styles/Http404.module.css';
+
+export default function NotFoundPage() {
+  return (
+    <BaseLayout>
+      <section className={styles.section}>
+        <h1>Page non trouvée</h1>
+
+        <p>
+          La page que vous cherchez n'existe pas. Elle s'est sûrement perdue lors d'une mise à jour de ce site.
+        </p>
+      </section>
+    </BaseLayout>
+  );
+}
