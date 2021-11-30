87883ec4bf5c0f64d8ca75424e18e9642096eb67
Hervé Cauwelier <herve.cauwelier@beta.gouv.fr>
2021-11-30 11:16:04 +0100

    bump dsfr to 1.2.1
    SPA seems to work again, at least in dev mode.
    
    Some spacing change but nothing troublesome.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,34 +2,11 @@
 
 import React from 'react';
 
-import { useRouter } from 'next/router';
-
 import '@/styles/globals.css';
-import '@gouvfr/dsfr/dist/css/dsfr.min.css';
+import '@gouvfr/dsfr/dist/dsfr/dsfr.min.css';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
   React.useEffect(() => import('@gouvfr/dsfr/src/main'), []);
-
-  /*
-   * Disable client-side transition.
-   *
-   * Framework dsfr is broken with SPA. If you attempt to load the page and
-   * click on the link, it is not possible to click on the menu on mobile.
-   *
-   * As a work around, we force a full browser reload on page change.
-   */
-  React.useEffect(() => {
-    const handleRouteChange = (url) => {
-      window.location.href = url;
-    };
-
-    router.events.on('routeChangeStart', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChange);
-    };
-  }, []);
 
   return (
     <>
