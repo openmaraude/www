5d16b0206364a4916cea9a509930f9e25be5706c
Julien Castets <castets.j@gmail.com>
2021-12-06 12:07:46 +0100

    Setup crisp chatbox

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 
+import Script from 'next/script';
+
 import '@/styles/globals.css';
 import '@gouvfr/dsfr/dist/dsfr/dsfr.min.css';
 
@@ -10,6 +12,34 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.$crisp = [];
+            window.CRISP_WEBSITE_ID = "${process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID}";
+
+            CRISP_RUNTIME_CONFIG = {
+              locale : "fr"
+            };
+
+            // Disable the following console warning:
+            //
+            //   [WARNING] Crisp found shims of native JavaScript methods. This
+            //   can alter the chatbox behavior and break things. Make sure not
+            //   to override listed functions to ensure your chatbox works as
+            //   expected. You may be looking for other JavaScript libraries in
+            //   use on this page.
+            window.$crisp.push(["safe", true])
+          `,
+        }}
+      />
+
+      <Script
+        src="https://client.crisp.chat/l.js"
+        strategy="afterInteractive"
+      />
+
       <Component {...pageProps} />
     </>
   );
