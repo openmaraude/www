c7e197b155e281ea4a9e19c6031a184e4a17c7b7
Julien Castets <castets.j@gmail.com>
2022-01-12 11:16:53 +0100

    Eslint

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -43,20 +43,22 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
 
       {/* Matomo tracking */}
-      <Script id="matomo-script" strategy="afterInteractive">{`
-        var _paq = window._paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(["setExcludedQueryParams", ["simulationId"]]);
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-          var u="https://stats.data.gouv.fr/";
-          _paq.push(['setTrackerUrl', u+'matomo.php']);
-          _paq.push(['setSiteId', '18']);
-          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-        })();
-      `}</Script>
+      <Script id="matomo-script" strategy="afterInteractive">
+        {`
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(["setExcludedQueryParams", ["simulationId"]]);
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="https://stats.data.gouv.fr/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '18']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        `}
+      </Script>
     </>
   );
 }
