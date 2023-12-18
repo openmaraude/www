0fd981f8edc1c19ab6231157dd703ab64f2c8d3c
Hervé Cauwelier <herve.cauwelier@beta.gouv.fr>
2023-12-18 14:57:42 +0100

    use our dedicated Matomo instance

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -52,9 +52,9 @@ function MyApp({ Component, pageProps }) {
           _paq.push(['trackPageView']);
           _paq.push(['enableLinkTracking']);
           (function() {
-            var u="https://stats.data.gouv.fr/";
+            var u="https://stats.beta.gouv.fr/";
             _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '18']);
+            _paq.push(['setSiteId', '4']);
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
             g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
           })();
